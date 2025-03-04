845f79c9a79717a22dcb44d8a16b4cad42458d4a
ShreyashDhamane <shreyashb.dhamane0@gmail.com>
2025-03-03 20:33:09 -0500

    test

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-
+#comment
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
